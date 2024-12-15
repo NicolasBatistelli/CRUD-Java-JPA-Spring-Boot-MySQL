83c3bac8ea8b6202ce017f31d9787217171dfdc4
Nicolas <nicolasbati8@gmail.com>
2024-12-15 12:51:35 -0300

    subiendo api spring boot

---
+++ src/main/java/com/example/api_java/models/Persona.java
@@ -0,0 +1,26 @@
+package com.example.api_java.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+public class Persona {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
+    private long id;
+
+    @Column
+    @Getter
+    @Setter
+    private String nombre;
+
+    @Column(name="tel")
+    @Getter
+    @Setter
+    private String telefono;
+
+}
