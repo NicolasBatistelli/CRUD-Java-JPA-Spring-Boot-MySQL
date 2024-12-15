af6778047472eb675c2a7570be9541bb44f55129
Nicolas <nicolasbati8@gmail.com>
2024-12-15 14:27:06 -0300

    subiendo tests

--- src/main/java/com/example/api_java/models/Persona.java
+++ src/main/java/com/example/api_java/models/Persona.java
@@ -4,23 +4,19 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 public class Persona {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Getter
-    @Setter
     private long id;
 
     @Column
-    @Getter
-    @Setter
     private String nombre;
 
     @Column(name="tel")
-    @Getter
-    @Setter
     private String telefono;
 
 }
